9f9745bea0158c809c969b0596cec8e1382f5f54
PaulinaBc <a6521150004@utch.edu.mx>
2024-06-03 10:37:10 -0600

    :(

---
+++ lib/Pantallas/dado.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dado extends StatefulWidget {
+  const Dado({super.key});
+
+  @override
+  State<Dado> createState() => _DadoState();
+}
+
+class _DadoState extends State<Dado> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
